4e9d6f5b5b09fe628705786dfc09d63753529652
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-02 12:12:39 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attr_encrypted :key, key: 'This is a key that is 256 bits!!'
 end
